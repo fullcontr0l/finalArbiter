5902b8d954b0fbf3eb59b042ea29f5f16b9c7eb9
fullcontr0l <rany.kob@gmail.com>
2023-08-09 02:54:09 +0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+/*  Navbar Toggle */
+
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
-}
+;}
 
+/* End of Navbar Toggle */
+
+/* Scroll Reveal */
 const sr = ScrollReveal ({
     distance: '65px',
     duration: 2600,
@@ -17,3 +22,15 @@ sr.reveal('.hero-text', {delay:200, origin:'top'});
 sr.reveal('.hero-img', {delay:450, origin:'top'});
 sr.reveal('.icons', {delay:500, origin:'left'});
 sr.reveal('.scroll-down', {delay:500, origin:'right'});
+
+/* End Scroll Reveal */
+
+/* Auto type */
+
+var typed = new Typed(".auto-type", {
+    strings: ["Matches", "Games", "Eating"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
+/* End of Auto type */

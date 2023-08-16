9005646663bcbf6a5cb231ac5c80bfe5664a447c
fullcontr0l <rany.kob@gmail.com>
2023-08-16 04:39:45 +0300

    .

---
+++ assets/roadmap.js
@@ -0,0 +1,52 @@
+
+/*  Navbar Toggle */
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+;}
+
+/* End of Navbar Toggle */
+
+
+
+
+
+
+/* Our process section */
+
+$(".step").click( function() {
+	$(this).addClass("active").prevAll().addClass("active");
+	$(this).nextAll().removeClass("active");
+});
+
+$(".step01").click( function() {
+	$("#line-progress").css("width", "3%");
+	$(".discovery").addClass("active").siblings().removeClass("active");
+});
+
+$(".step02").click( function() {
+	$("#line-progress").css("width", "25%");
+	$(".strategy").addClass("active").siblings().removeClass("active");
+});
+
+$(".step03").click( function() {
+	$("#line-progress").css("width", "50%");
+	$(".creative").addClass("active").siblings().removeClass("active");
+});
+
+$(".step04").click( function() {
+	$("#line-progress").css("width", "75%");
+	$(".production").addClass("active").siblings().removeClass("active");
+});
+
+$(".step05").click( function() {
+	$("#line-progress").css("width", "100%");
+	$(".analysis").addClass("active").siblings().removeClass("active");
+});
+
+
+/* End of Our process section */
---
+++ assets/script.js
@@ -0,0 +1,72 @@
+
+/*  Navbar Toggle */
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+;}
+
+/* End of Navbar Toggle */
+
+/* Scroll Reveal */
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.hero-text', {delay:200, origin:'top'});
+sr.reveal('.hero-img', {delay:450, origin:'top'});
+sr.reveal('.icons', {delay:500, origin:'left'});
+sr.reveal('.scroll-down', {delay:500, origin:'right'}); 
+
+/* End Scroll Reveal */
+
+/* Auto type */
+
+var typed = new Typed(".auto-type", {
+    strings: ["Matches", "Games", "Teams"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+});
+/* End of Auto type */
+
+
+
+/* Our process start */
+$(".step").click( function() {
+	$(this).addClass("active").prevAll().addClass("active");
+	$(this).nextAll().removeClass("active");
+});
+
+$(".step01").click( function() {
+	$("#line-progress").css("width", "3%");
+	$(".discovery").addClass("active").siblings().removeClass("active");
+});
+
+$(".step02").click( function() {
+	$("#line-progress").css("width", "25%");
+	$(".strategy").addClass("active").siblings().removeClass("active");
+});
+
+$(".step03").click( function() {
+	$("#line-progress").css("width", "50%");
+	$(".creative").addClass("active").siblings().removeClass("active");
+});
+
+$(".step04").click( function() {
+	$("#line-progress").css("width", "75%");
+	$(".production").addClass("active").siblings().removeClass("active");
+});
+
+$(".step05").click( function() {
+	$("#line-progress").css("width", "100%");
+	$(".analysis").addClass("active").siblings().removeClass("active");
+});
+
+/* Our process end */
